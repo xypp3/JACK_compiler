674f910554e1c589c764c7c7878107c2e113e3f9
xypp3 <petrkmilev@gmail.com>
2023-02-17 00:21:36 +0000

    feat: Helper func like is_lowercase, is_valid_identifier and is_comment

--- lexer.c
+++ lexer.c
@@ -12,10 +12,8 @@ program online or share it with anyone without permission of the module leader.
 
 Student Name: Petr-Konstantin Milev
 Student ID: sc21pkm
-Email:
-sc21pkm@leeds.ac.uk
-Date Work Commenced:
-2023-Feb-09
+Email: sc21pkm@leeds.ac.uk
+Date Work Commenced: 2023-Feb-09
 *************************************************************************/
 
 #include "lexer.h"
@@ -35,11 +33,16 @@ typedef struct {
   int top;
 } Stack;
 
-typedef enum comment_types { NOT_COMMENT, ONELINE, MULTILINE } Comment_Types;
+typedef enum comment_types {
+  NOT_STARTED,
+  NOT_COMMENT,
+  ONELINE,
+  MULTILINE
+} Comment_Types;
 
 FILE *input_file;
-FILE *output_file;
 Stack peek_str;
+int newline_flag;
 
 /**************************************
  **************************************
@@ -86,20 +89,40 @@ int pop(int *pop_val, Stack *stack) {
  ***************lexer******************
  **************************************
  **************************************/
-int peek(FILE *fptr) {
+int peek_char(FILE *fptr) {
   int peek_char = fgetc(fptr);
   return (peek_char == EOF) ? EOF : ungetc(peek_char, fptr);
 }
 
-int is_white_space(int next_char) {
+int is_white_space(int next_char, int *newline_flag) {
+  newline_flag += next_char == '\n';
+
   return next_char == ' ' || next_char == '\n' || next_char == '\t' ||
          next_char == '\r';
 }
 
+int is_lowercase(int character) {
+  return 97 <= character && character <= 122; // a <= character <= z
+}
+
+int is_uppercase(int character) {
+  return is_lowercase(character + 32); // uppercase char + 32 == lowercase char
+}
+
+int is_digit(int character) {
+  return 48 <= character && character <= 57; // 0 <= character <= 9
+}
+
+int is_valid_identifier(int character, unsigned int position) {
+  return (is_lowercase(character) || is_uppercase(character) ||
+          character == '_') ||
+         (position != 0 && is_digit(character));
+}
+
 int is_comment(int next_char, Comment_Types *is_comment_started) {
-  if (is_comment_started == NOT_COMMENT) {
+  if (is_comment_started == NOT_STARTED) {
     if (next_char == '/') {
-      switch (peek(input_file)) {
+      switch (peek_char(input_file)) {
       case ('/'):
         *is_comment_started = ONELINE;
         return TRUE;
@@ -107,15 +130,38 @@ int is_comment(int next_char, Comment_Types *is_comment_started) {
         *is_comment_started = MULTILINE;
         return TRUE;
       default:
-        return FALSE;
+        *is_comment_started = NOT_COMMENT;
+        return FALSE; // means is division/ multiplication or user error
       }
     } else {
+      // cause first char isn't a backslash
+      *is_comment_started = NOT_COMMENT;
       return FALSE;
     }
   } else {
-    // TODO: multiline case
-    // TODO: oneline case
-    // TODO: error case
+
+    // TODO:
+    if (next_char == EOF) {
+    }
+
+    switch (*is_comment_started) {
+    case ONELINE:
+      if (next_char == '\n') {
+        newline_flag++;
+        return FALSE;
+      }
+
+      return TRUE;
+    case MULTILINE:
+      if (next_char == '*' && peek_char(input_file) == '/') {
+        return FALSE;
+      }
+
+      return TRUE;
+    default:
+      printf("Error has occured, in is_comment()");
+      return FALSE;
+    }
   }
   return FALSE;
 }
@@ -139,6 +185,7 @@ int InitLexer(char *file_name) {
 
   // init stack for PeekNextToken
   stack_init(&peek_str, MAX_LEXEM_SIZE);
+  newline_flag = 0;
 
   return TRUE;
 }
@@ -152,26 +199,23 @@ Token GetNextToken() {
   push(next_char, &peek_str);
 
   /* skip all white space (' ', '\n', '\t', '\r') */
-  while (is_white_space(next_char)) {
-    push(next_char, &peek_str);
+  while (is_white_space(next_char, &newline_flag)) {
     next_char = fgetc(input_file);
+    push(next_char, &peek_str);
   }
   /* skip all comments */
   // only consider this var for the following loop
-  Comment_Types comment_has_begun = NOT_COMMENT;
-  while (1) {
-    int comment_code = is_comment(next_char, &comment_has_begun);
-    if (comment_code == TRUE) {
-      push(next_char, &peek_str);
-      next_char = fgetc(input_file);
-    } else if (comment_code == EOF &&
-               comment_has_begun !=
-                   NOT_COMMENT) { // second condition may be redundant
-      // TODO: EOF IN COMMENT ERROR
-      break;
-    } else {
-      break;
-    }
+  Comment_Types comment_has_begun = NOT_STARTED;
+  while (is_comment(next_char, &comment_has_begun)) {
+    next_char = fgetc(input_file);
+    push(next_char, &peek_str);
+    newline_flag += next_char == '\n';
+  }
+
+  /* get EOF */
+  if (next_char == EOF) {
+    push(next_char, &peek_str);
+    token.tp = EOFile;
   }
 
   token.tp = ERR;
@@ -184,6 +228,7 @@ Token PeekNextToken() {
   Token token;
   // all chars should be on stack
   token = GetNextToken();
+  // place back all characters in the right order
   while (peek_str.top > -1) {
     int prev_char; // tmp character
     pop(&prev_char, &peek_str);
@@ -191,15 +236,14 @@ Token PeekNextToken() {
     ungetc(prev_char, input_file);
   }
 
-  return t;
+  return token;
 }
 
 // clean out at end, e.g. close files, free memory, ... etc
 int StopLexer() {
 
   // FREE global vars
-  free(input_file);
-  free(output_file);
+  fclose(input_file);
   free(peek_str.stack);
 
   return TRUE;
@@ -207,10 +251,28 @@ int StopLexer() {
 
 // do not remove the next line
 #ifndef TEST
-int main() {
-  // implement your main function here
-  // NOTE: the autograder will not use your main function
+// NOTE: main() NOT tested by autograder
+int main(int argc, char **argv) {
 
+  if (argc != 2) {
+    printf("Usage: lexer \"filename\"");
+    return FALSE;
+  }
+
+  FILE *output_file;
+  char *output_filename = strcat(argv[1], "_tokens_mine.txt");
+
+  if ((output_file = fopen(output_filename, "w")) == NULL) {
+    printf("Error! opening file");
+
+    fclose(output_file);
+    // Program exits if the file pointer returns NULL.
+    return FALSE;
+  }
+
+  InitLexer(argv[1]);
+
+  fclose(output_file);
   return TRUE;
 }
 // do not remove the next line
