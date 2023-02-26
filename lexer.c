06626b087ebe438b47136743c72991233392450b
xypp3 <petrkmilev@gmail.com>
2023-02-26 15:07:40 +0000

    Revert "refactor: type changing"
    This reverts commit 9613aab676c00dc9cb3012d6d37ccb88013e1a64.

--- lexer.c
+++ lexer.c
@@ -30,8 +30,8 @@ Date Work Commenced: 2023-Feb-09
 
 typedef struct {
   int *stack;
-  unsigned int size;
-  unsigned int top;
+  int size;
+  int top;
 } Stack;
 
 typedef enum comment_types {
@@ -71,7 +71,6 @@ int static stack_init(Stack *stack, int size) {
 int push(int number, Stack *stack) {
   stack->top += 1;
 
-  // reallocation
   if (stack->top >= stack->size) {
     stack->stack =
         (int *)realloc(stack->stack, (sizeof(int) * stack->size * 2));
@@ -84,7 +83,6 @@ int push(int number, Stack *stack) {
     stack->size *= 2;
   }
 
-  // push var to top
   stack->stack[stack->top] = number;
   return TRUE;
 }
